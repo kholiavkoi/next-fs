06c2b29387fb5d49d59a0e5766b319a973319fd9
kholiavkoi <76649658+kholiavkoi@users.noreply.github.com>
2023-06-06 20:17:59 +0300

    added dark/light mode

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from "next/link";
 import styles from './Navbar.module.css'
+import DarkModeToggle from "@/components/DarkModeToggle/DarkModeToggle";
 
 const links = [
 	{
@@ -40,6 +41,7 @@ const Navbar = () => {
 		<div className={styles.container}>
 			<Link href='/' className={styles.logo}>IKholiavko</Link>
 			<div className={styles.links}>
+				<DarkModeToggle />
 				{links.map(link => (
 					<Link  key={link.id} href={link.url} className={styles.link}>{link.title}</Link>
 				))}
