e5562e8e314dd4c48903e1ebb6382c4accc79e54
kholiavkoi <76649658+kholiavkoi@users.noreply.github.com>
2023-06-07 23:14:35 +0300

    added register

---
+++ src/app/api/auth/register/route.js
@@ -0,0 +1,29 @@
+import connect from "@/utils/db";
+import { NextResponse } from "next/server";
+import User from "@/models/User";
+import bcrypt from 'bcryptjs'
+
+export const POST = async (request) => {
+	const { name, email, password } = await request.json()
+
+	await connect()
+
+	const hashedPassword = await bcrypt.hash(password, 5)
+
+	const newUser = new User({
+		name,
+		email,
+		password: hashedPassword
+	})
+
+	try {
+		await newUser.save()
+		return new NextResponse('User has been created', {
+			status: 201
+		})
+	} catch (e) {
+		return new NextResponse(e.message, {
+			status: 500
+		})
+	}
+}
