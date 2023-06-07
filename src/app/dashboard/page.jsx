a7db0ee9070a00e985bbd9bb1c99afe8c0230e43
kholiavkoi <76649658+kholiavkoi@users.noreply.github.com>
2023-06-07 22:20:53 +0300

    added auth with google

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,11 @@
+'use client'
 import React from 'react';
 import styles from './page.module.css'
+import { useSession } from "next-auth/react";
 
 const Dashboard = () => {
+	const session = useSession()
+	console.log(session)
 	return (
 		<div className={styles.container}>
 			Dashboard
